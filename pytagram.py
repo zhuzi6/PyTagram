6069c3341daea159626e7dab1abdee675d414e0a
aytss23 <75861825+aytss23@users.noreply.github.com>
2021-07-13 16:27:00 +0300

    Update pytagram.py

--- pytagram.py
+++ pytagram.py
@@ -3,16 +3,16 @@ from instaloader import Post
 
 
 class PyTagram:
+
     def __init__(self):
+        self.downloadProcces = True
         self.pyTagram = instaloader.Instaloader()
-        #self.logIn()
     
     #login 
     def logIn(self):
 
         print("--- LOGIN ---")
-        print("Dont forget disable the 2FA!!")
-
+        
         userNickname = str(input("username : "))
         userPassword = str(input("password :"))
 
@@ -22,7 +22,9 @@ class PyTagram:
     def proccesMenu(self):
         print("--- PyTagram ---")
         print("Downlaod Profile Pictures : 1")
-        print("Exit : 0")
+        print("Download Saved Posts :      2")
+        print("Download Post :             3")
+        print("Exit :                      0")
 
         userInput = int(input("Procces : "))
 
@@ -32,14 +34,21 @@ class PyTagram:
         if userInput == 1:
             self.downloadProfilePicture()
 
+        if userInput == 2:
+            self.downloadSavedPosts()
+        
+        if userInput == 3:
+            self.downloadPost()
+
     #download the profile pictures
     def downloadProfilePicture(self):
+        
         targetNickName = str(input("nickname : ")) 
         
         print("[DOWNLOAD] - Started")
 
         try:
-            downloadProcces = self.pyTagram.download_profile(targetNickName, profile_pic_only=True)
+            self.downloadProcces = self.pyTagram.download_profile(targetNickName, profile_pic_only=True)
 
         except Exception as DownlaodError:
             print("[DOWNLOAD] - Failed")
@@ -47,15 +56,32 @@ class PyTagram:
         else:
             print("[DOWNLAOAD] - Succesfull")
 
-    
+    def downloadSavedPosts(self):
+        try:
+            self.downloadProcces = self.pyTagram.download_saved_posts()
+        
+        except Exception as DownlaodError:
+            print("[DOWNLOAD] - Failed")
 
+        else:
+            print("[DOWNLAOAD] - Succesfull")
+
+    def downloadPost(self):
+
+        targetUserNickName = str(input("nickname : "))
+        targetPostShortCode = str(input("shortcode : "))
+
+        try:
+            self.downloadProcces = self.pyTagram.download_post(targetPostShortCode, targetUserNickName)
+
+        except instaloader.PrivateProfileNotFollowedException or instaloader.ProfileNotExistsException as DownloadException:
+            print(f"[DOWNLOAD] - Failed | Target profile not found or not following")
+
+         
 #main
 #||-------------------------------------------------||
 _pyTagram = PyTagram()
+_pyTagram.logIn()
 while True:
     _pyTagram.proccesMenu()
 
-
-#TO DO 
-# Append Downloading Posts and Stories 
-# Append 
